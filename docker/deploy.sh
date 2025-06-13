30a5a0666c1ab7c031a95a6fa0223da39ddd0ea5
vp01273 <vp01273@globalymc.com>
2025-06-13 08:41:08 +0800

    fix: 简化部署脚本，合并命令以提高可读性和执行效率

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,8 +3,7 @@
 COMPONENT=$1
 
 
-ssh root@10.191.6.215 "
-cd /home/ubuntu/deploy/yCopilot-Core/docker 
-&& git pull
-&& docker compose pull $VERSION
-&& docker compose up -d $COMPONENT"
+ssh root@10.191.6.215 "cd /home/ubuntu/deploy/yCopilot-Core/docker && \
+git pull && \
+docker compose pull && \
+docker compose up -d $COMPONENT"
